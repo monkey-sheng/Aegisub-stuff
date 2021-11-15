54166481f9193b59ddcfe4533464fb3d1fc41817
monkey-sheng <32032155+monkey-sheng@users.noreply.github.com>
2021-11-15 12:19:07 +0000

    nvenc usage changed, adapt to new usage

--- encode.py
+++ encode.py
@@ -1,5 +1,6 @@
+import json
 from os import listdir
-from subprocess import run
+from subprocess import PIPE, run
 import os
 
 files = listdir()
@@ -13,8 +14,15 @@ if len(videos) == 0 or len(subs) == 0:
     exit(1)
 
 video, ass = videos[0], subs[0]
+
+probe = run(['ffprobe', '-print_format', 'json', '-show_streams', video],
+            stdout=PIPE)
+info = json.loads(probe.stdout)
+# maintain same bitrate
+bitrate = str(int(int(info['streams'][0]['bit_rate']) / 1000)) + 'k'
+
 f_out = '【已压】' + video
 out_name = os.path.join(os.path.expanduser('~'), 'Desktop', f_out)
 print(out_name)
-run(['ffmpeg', '-i', video, '-pix_fmt', 'yuv420p', '-vf',
-     f'subtitles={ass}', '-c:a', 'copy', '-c:v', 'h264_nvenc', '-profile:v', 'high', '-level', '5.0', '-cq', 20, out_name])
+run(['ffmpeg', '-i', video, '-pix_fmt', 'yuv420p', '-vf', f'subtitles={ass}', '-c:a', 'copy',
+'-c:v', 'h264_nvenc', '-b:v', bitrate, '-profile:v', 'high', '-level', '5.1', '-rc', 'vbr', '-tune', 'hq', '-rc-lookahead', '16', '-b_ref_mode', 'middle', out_name])
