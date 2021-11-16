2266beefe35326935a2fcb5909bc079b113090e7
monkey-sheng <32032155+monkey-sheng@users.noreply.github.com>
2021-11-16 16:19:20 +0000

    now supports drag and drop files for encoding

--- encode.py
+++ encode.py
@@ -2,24 +2,41 @@ import json
 from os import listdir
 from subprocess import PIPE, run
 import os
+from sys import argv
+from time import sleep
+from sys import exit
 
-files = listdir()
-videos = [f for f in files if f.endswith('.mp4')]
-subs = [f for f in files if f.endswith('.ass')]
-if len(videos) == 0 or len(subs) == 0:
-    from time import sleep
-    from sys import exit
-    print('no .mp4 and/or .ass found. Quitting')
+
+if len(argv) == 1:
+    files = listdir()
+    videos = [f for f in files if f.endswith(('mp4', 'mov', 'webm'))]
+    subs = [f for f in files if f.endswith('.ass')]
+    if len(videos) == 0 or len(subs) == 0:
+        print('no video and/or .ass found. Quitting')
+        sleep(5)
+        exit(1)
+    video, ass = videos[0], subs[0]
+elif len(argv) == 3:
+    video, ass = (argv[1], argv[2]) if argv[1].endswith(('mp4', 'mov', 'webm')) and argv[2].endswith('ass') else (argv[2], argv[1]) if argv[2].endswith(('mp4', 'mov', 'webm')) and argv[1].endswith('ass') else (None, None)
+    if not all([video, ass]):
+        print('unrecognized extensions, please double check')
+        sleep(5)
+        exit(1)
+elif len(argv) > 3:
+    print('too many files, can only do 1 video/ass at a time')
     sleep(5)
     exit(1)
-
-video, ass = videos[0], subs[0]
+else:
+    print('UNKNOWN ERROR')
+    sleep(5)
+    exit(1)
 
 probe = run(['ffprobe', '-print_format', 'json', '-show_streams', video],
             stdout=PIPE)
 info = json.loads(probe.stdout)
-# maintain same bitrate
-bitrate = str(int(int(info['streams'][0]['bit_rate']) / 1000)) + 'k'
+# maintain same bitrate for video stream
+video_stream = list(filter(lambda s: s['codec_type'] == 'video', info['streams']))[0]
+bitrate = str(int(video_stream['bit_rate']) / 1000) + 'k'
 
 f_out = '【已压】' + video
 out_name = os.path.join(os.path.expanduser('~'), 'Desktop', f_out)
